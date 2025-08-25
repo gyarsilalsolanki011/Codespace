6c368d51552f5e70913194c4c131d5689bb56c15
Gyarsilal Solanki <gyarsilalsolanki011@gmail.com>
2025-08-26 01:27:54 +0530

    package changes

--- Java/patterns/FullTrianglePattern.java
+++ Java/patterns/FullTrianglePattern.java
@@ -1,15 +1,21 @@
+package patterns;
 import java.util.Scanner;
 
-public class HalfTrianglePattern {
+public class FullTrianglePattern {
     public static void main(String[] args) {
-        System.out.print("Enter the number of rows for the half triangle pattern: ");
+        System.out.print("Enter the number of rows for the full triangle pattern: ");
         Scanner scanner = new Scanner(System.in);
         int raw = scanner.nextInt();
         scanner.close();
 
         for (int i = 1; i <= raw; i++) {
 
-            int stars = i;
+            int spaces = raw - i;
+            for (int space = 1; space <= spaces; space++) {
+                System.out.print(" ");
+            }
+
+            int stars = (2 * i) - 1;
             for (int star = 1; star <= stars; star++) {
                 System.out.print("*");
             }
--- Java/patterns/HalfTrianglePattern.java
+++ Java/patterns/HalfTrianglePattern.java
@@ -1,20 +1,16 @@
+package patterns;
 import java.util.Scanner;
 
-public class FullTrianglePattern {
+public class HalfTrianglePattern {
     public static void main(String[] args) {
-        System.out.print("Enter the number of rows for the full triangle pattern: ");
+        System.out.print("Enter the number of rows for the half triangle pattern: ");
         Scanner scanner = new Scanner(System.in);
         int raw = scanner.nextInt();
         scanner.close();
 
         for (int i = 1; i <= raw; i++) {
 
-            int spaces = raw - i;
-            for (int space = 1; space <= spaces; space++) {
-                System.out.print(" ");
-            }
-
-            int stars = (2 * i) - 1;
+            int stars = i;
             for (int star = 1; star <= stars; star++) {
                 System.out.print("*");
             }
